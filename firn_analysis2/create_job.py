290602fb0a0a94bee8dfea582dae1448e48e6bc9
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2024-02-19 13:43:48 -0500

    create job fo all

--- firn_analysis2/create_job.py
+++ firn_analysis2/create_job.py
@@ -1,14 +1,16 @@
 import sys
-import ku_scripting
+import osc_scripting
 import os
+import pathlib
 import configparser
 
-from ku_scripting import make_job
+from osc_scripting import make_job
 
 cluster_config = sys.argv[1]
-
+config_path = pathlib.Path(__file__).parent.absolute() / cluster_config
 config = configparser.ConfigParser()
-config.read(cluster_config)
+print(config_path)
+config.read(config_path)
 
 #Cluster Settings
 cluster_settings = config['CLUSTER']
@@ -22,7 +24,19 @@ HOURS = int(cluster_settings['HOURS'])
 MEMORY = int(cluster_settings['MEMORY']) # in MB
 TASKS = int(cluster_settings['TASKS'])
 NCPUS = int(cluster_settings['NCPUS'])
-print('TASKS=', TASKS, 'NPCUS=', NCPUS)
+
+if 'ACCOUNT' in cluster_settings.keys(): #the account has to be specified by osc, check if memeber
+    ACCOUNT=cluster_settings['ACCOUNT']
+else:
+    print('error, add ACCOUNT as a field to the cluster config', sys.argv[1])
+    print('checking keys:', cluster_settings.keys())
+    sys.exit()
+if 'EMAIL' in cluster_settings.keys():
+    EMAIL = cluster_settings['EMAIL']
+else:
+    EMAIL='NONE'
+
+#print('TASKS=', TASKS, 'NPCUS=', NCPUS)
 
 job_settings = config['JOB']
 operator = job_settings['operator']
@@ -38,4 +52,4 @@ fname_out = prefix + '.out'
 job_name = prefix
 make_job(fname_shell=fname_shell, fname_outfile=fname_out, jobname=job_name, command=cmd,
          nNodes_min=NODES_MIN, nNodes_max=NODES_MAX, partition=PARTITION,
-         days=DAYS, hours=HOURS, nodeMemory=MEMORY, tasks = TASKS, ncpus=NCPUS)
+         days=DAYS, hours=HOURS, nodeMemory=MEMORY, tasks = TASKS, ncpus=NCPUS, account=ACCOUNT, email=EMAIL)
