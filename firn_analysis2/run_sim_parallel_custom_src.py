f827047deb7f1f1f1a05e45f9f2aa51a02f7528b
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2024-05-09 21:14:12 -0100

    custom source

---
+++ firn_analysis2/run_sim_parallel_custom_src.py
@@ -0,0 +1,265 @@
+import numpy as np
+import math
+import h5py
+import sys
+import configparser
+from mpi4py import MPI
+import meep as mp
+import os
+from util import get_data, findNearest
+import util
+
+comm = MPI.COMM_WORLD
+
+if len(sys.argv) == 2:
+    fname_config = sys.argv[1]
+else:
+    fname_config = 'config_in.txt'
+
+print('Initiating Simulation of a Gaussian pulse generated from a point source inside a modelled ice-sheet (range-independent)')
+
+config_file = os.path.join(os.path.dirname(__file__), fname_config)
+config = configparser.ConfigParser()
+config.read(config_file)
+
+#Input Parameters
+print('Input Parameters from config file', fname_config)
+
+geometry = config['GEOMETRY']
+print('Geometry:')
+for key in geometry.keys():
+    print(key, geometry[key])
+transmitter = config['TRANSMITTER']
+print('Transmitter:')
+for key in transmitter.keys():
+    print(key, transmitter[key])
+settings = config['SETTINGS']
+print('settings:')
+for key in settings.keys():
+    print(key, settings[key])
+receiver = config['RECEIVER']
+print('receiver:')
+for key in receiver.keys():
+    print(key, receiver[key])
+ref_index = config['REFRACTIVE INDEX']
+print('ref index')
+for key in ref_index.keys():
+    print(key, ref_index[key])
+
+fname_prefix = settings['prefix']
+fname_data = ref_index['data_file']
+fname_rx_list = receiver['rx_list']
+
+rx_list = np.genfromtxt(fname_rx_list)
+
+nprof_sp, zprof_sp = get_data(fname_data)
+
+nair = 1.0      # index of air
+iceDepth = float(geometry['iceDepth'])  # depth of ice
+airHeight = float(geometry['airHeight'])     # height of air
+iceRange = float(geometry['iceRange']) #radius of domain
+boreholeRadius = float(geometry['boreholeRadius']) #radius of borehole
+pad = float(geometry['pad'])
+
+sourceDepth = float(transmitter['sourceDepth']) #depth of diple below ice
+sourceRange = float(transmitter['sourceRange']) + boreholeRadius/2
+
+print('sourceDepth', sourceDepth)
+#Frequency
+frequency = float(transmitter['frequency']) #MHz
+bandwidth = float(transmitter['bandwidth'])
+c_meep = 300.0 # m/us
+wavelength = c_meep/frequency #m.MHz or m / us
+freq_meep = 1/wavelength #Meep Units
+
+wavelength_band = c_meep/bandwidth
+band_meep = 1/wavelength_band
+
+sourceAmp = float(transmitter['sourceAmp'])
+R_tot = iceRange + pad
+
+R_cent = boreholeRadius/2 + R_tot/2
+
+Z_tot = iceDepth + airHeight + 2*pad
+H_aircent = airHeight/2 # Central Height of Air
+Z_icecent = iceDepth/2 # Central Depth of Ice
+r_cent = boreholeRadius/2
+iceRange_wo_bh = R_tot-boreholeRadius #size of Ice Block
+
+nice = 1.78
+resolution = 12.5	#how many pixels per meter
+mpp = 1/resolution    # meters per pixel
+column_size = iceRange/mpp	# number of pixels in the column
+vice = 1/nice   	# phase velocity in ice
+t_start = R_tot/vice # approximate time to reach steady state
+#TODO: Check, t_start can be shorter
+
+print('r_tx=', sourceRange)
+print('H_aircent', H_aircent)
+print('Z_icecent', Z_icecent)
+def nProfile_func(R):
+    z = R[2]
+    A = 1.78
+    B = 0.43
+    C = 0.0132 #1/m
+    #return mp.Medium(index=A-B*math.exp(-C*(z + Z_tot/2 - H_air)))
+    return mp.Medium(index= A - B * math.exp(-C * z))
+
+def nProfile_data(R, zprof_data=zprof_sp, nprof_data=nprof_sp):
+    z = R[2]
+    ii_z = findNearest(zprof_data, z)
+    n_z = nprof_data[ii_z]
+    return mp.Medium(index=n_z)
+##*********************************************************************##
+
+##**********************Simulation Setup*******************************##
+dimensions = mp.CYLINDRICAL
+
+pml_layers = [mp.PML(pad)]
+cell = mp.Vector3(2*R_tot, mp.inf, Z_tot)
+
+
+'''
+    mp.Block(center=mp.Vector3(r_cent, 0, Z_icecent),
+             size=mp.Vector3(boreholeRadius, mp.inf, iceDepth),
+             material=mp.Medium(index=nair)),
+'''
+#TODO: Check -> Change remove borehole from geometry
+geometry_dipole = [
+    mp.Block(center=mp.Vector3(R_cent, 0, H_aircent), #AIR
+             size=mp.Vector3(iceRange, mp.inf, airHeight),
+             material=mp.Medium(index=nair)),
+    mp.Block(center=mp.Vector3(R_cent, 0, Z_icecent), #ICE
+             size=mp.Vector3(iceRange, mp.inf, iceDepth),
+             material=nProfile_data)
+]
+
+# create the source
+sources_dipole = []
+t_begin = 20.0
+
+#DEFINE SOURCE:
+def band_limited_pulse(t, t_start=t_begin, fmin=freq_meep-band_meep/2, fmax=freq_meep+band_meep/2, amp=sourceAmp):
+    nSamples = len(t)
+    dt = abs(t[1] - t[0])
+    fsamp = 1/dt
+    impulse = np.zeros(nSamples)
+    ii = util.findNearest(t, t_start)
+    impulse[ii] = amp
+
+    pulse_out = util.butterBandpassFilter(impulse, fmin, fmax, fsamp)
+    print(pulse_out)
+    return pulse_out
+
+source1 = mp.Source(mp.CustomSource(src_func = lambda t: band_limited_pulse()),
+                    component=mp.Ez,
+                    center=mp.Vector3(sourceRange, 0, sourceDepth),
+                    size=mp.Vector3(0,0,0))
+
+sources_dipole.append(source1)
+# create simulation
+sim_dipole = mp.Simulation(force_complex_fields=True,
+                cell_size=cell,
+                dimensions=mp.CYLINDRICAL,
+                boundary_layers=pml_layers,
+                geometry=geometry_dipole,
+                sources=sources_dipole,
+                resolution=resolution)
+
+#=================================================================
+# Get RX Functions
+#================================================================
+rxList = []
+nRx = len(rx_list)
+for i in range(nRx):
+    rx_x = rx_list[i][0]
+    rx_z = rx_list[i][1]
+    pt_ij = mp.Vector3(rx_x, mp.inf, rx_z)
+    rxList.append(pt_ij)
+print(rxList)
+c_mns = 0.3 #Speed of Light in m / ns
+
+#TODO: Check time definition!
+dt_ns = 0.5 # ns
+dt_m = dt_ns * c_mns # Meep Units a / c
+Courant = 0.5
+
+dt_C = Courant * mpp # The simulation time step [a/c] -> dt = C dr , C : Courant Factor, C = 0.5 (default)
+# For accurate sims, C <= n_max / sqrt(nDimensions)
+
+nSteps = int(t_start/dt_C) + 10
+print(nSteps)
+pulse_rx_arr = np.zeros((nRx, nSteps),dtype='complex')
+tspace = np.linspace(0, t_start / c_mns, nSteps)
+def get_amp_at_t2(sim):
+    nRx = len(rxList)
+    factor = dt_m / dt_C
+    tstep = sim.timestep()
+    ii_step = int(float(tstep) / factor) - 1 #TODO: Check if this starts as 0 or 1
+    for i in range(nRx):
+        rx_pt = rxList[i]
+
+        amp_at_pt = sim.get_field_point(c=mp.Ez, pt=rx_pt)
+        '''
+        if rx_pt.z == sourceDepth and rx_pt.x == 20:
+            print('field at z = ', sourceDepth, 'm, E(z) =', amp_at_pt)
+        if rx_pt.z == -sourceDepth and rx_pt.x == 20:
+            print('field at z = ', -sourceDepth, 'm, E(z) =', amp_at_pt)
+        '''
+        pulse_rx_arr[i, ii_step] = amp_at_pt
+
+path2sim = settings['path2output']
+sim_dipole.init_sim()
+sim_dipole.use_output_directory(path2sim)
+sim_dipole.run(mp.at_every(dt_C, get_amp_at_t2),until=t_start)
+
+fname_out = path2sim + '/' + fname_prefix + 'z_tx_' + str(sourceDepth) + 'm_freq=' + str(frequency) + 'MHz_out.h5'
+
+for i in range(nRx):
+    print('check if all elements are zero', np.all(pulse_rx_arr[i] == 0), pulse_rx_arr[i])
+
+def add_data_to_hdf(hdf_in, label, dataset):
+    '''
+    if label in hdf_in.keys():
+        hdf_in[label] = dataset
+    else:
+        hdf_in.create_dataset(label, data=dataset)
+    '''
+    #Check if label exists
+    check_bool = label in hdf_in.keys()
+    if check_bool == False:
+        hdf_in.create_dataset(label, data=dataset)
+
+with h5py.File(fname_out, 'a', driver='mpio', comm=MPI.COMM_WORLD) as output_hdf:
+    output_hdf.attrs['iceDepth'] = iceDepth
+    output_hdf.attrs['airHeight'] = airHeight
+    output_hdf.attrs['iceRange'] = iceRange
+    output_hdf.attrs['boreholeRadius'] = boreholeRadius
+    output_hdf.attrs['sourceDepth'] = sourceDepth
+    output_hdf.attrs['frequency'] = frequency
+    output_hdf.attrs['sourceRange'] = sourceRange
+    output_hdf.attrs['pad'] = pad
+
+    rxList_out = []
+    for i in range(nRx):
+        rx_i = rxList[i]
+        rxList_out.append([rx_i.x, rx_i.z])
+    rx_label = 'rxList'
+    pulse_label = 'rxPulses'
+    tspace_label = 'tspace'
+    '''
+    Ez_label = 'Ez'
+    Er_label = 'Er'
+    eps_label = 'epsilon_r'
+    Ez_data = sim_dipole.get_array(center=mp.Vector3(), size=cell, component=mp.Ez)
+    Er_data = sim_dipole.get_array(center=mp.Vector3(), size=cell, component=mp.Er)
+    Eps_data = sim_dipole.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
+    '''
+
+    add_data_to_hdf(output_hdf, rx_label, rxList_out)
+    add_data_to_hdf(output_hdf, pulse_label, pulse_rx_arr)
+    #add_data_to_hdf(output_hdf, Ez_label, Ez_data)
+    #add_data_to_hdf(output_hdf, Er_label, Er_data)
+    add_data_to_hdf(output_hdf, tspace_label, tspace)
+    #add_data_to_hdf(output_hdf, eps_label, Eps_data)
+    output_hdf.close()
--- firn_analysis2/util.py
+++ firn_analysis2/util.py
@@ -2,7 +2,19 @@ import numpy as np
 import math
 from numpy.lib.format import open_memmap
 import meep as mp
+from scipy.signal import butter, lfilter
 
+def butterBandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butterBandpassFilter(data, lowcut, highcut, fs, order=3):
+    b, a = butterBandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 def get_data(fname_txt):
     n_data = np.genfromtxt(fname_txt)
     zprof = n_data[:,0]
@@ -13,6 +25,30 @@ def findNearest(x_arr, x):
     dx = abs(x_arr - x)
     ii = np.argmin(dx)
     return ii
+
+
+def select_rx_ii(rxList, x_in, z_in):
+    dr_list = []
+    for rx in rxList:
+        x_rx = float(rx[0])
+        z_rx = float(rx[1])
+        dr_sq = (x_rx-x_in)**2 + (z_rx - z_in)**2
+        dr = np.sqrt(dr_sq)
+        dr_list.append(dr)
+    ii_rx = np.argmin(dr_list)
+    return ii_rx
+
+def get_actual_rx(rxList, x_in, z_in):
+    ii_rx = select_rx_ii(rxList, x_in, z_in)
+    rx_actual = rxList[ii_rx]
+    x_rx = float(rx_actual[0])
+    z_rx = float(rx_actual[1])
+    return x_rx, z_rx
+
+def get_pulse(rxPulses, rxList, x_in, z_in):
+    ii_rx = select_rx_ii(rxList, x_in, z_in)
+    rx_pulse = rxPulses[ii_rx]
+    return rx_pulse
 
 def add_data_to_hdf(hdf_in, label, dataset):
     #Check if label exists
